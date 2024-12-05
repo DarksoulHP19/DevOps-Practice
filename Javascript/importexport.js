cb0202513a6d48eedd1679ec296eb53d2ecc6639
DarksoulHP19 <harshjpatel974@gmail.com>
2024-12-05 15:50:50 +0530

    Javascript completed

---
+++ Javascript/importexport.js
@@ -0,0 +1,7 @@
+//->import and export
+
+import { capitilizestring } from "./string_function"
+const cap = capitilizestring('hello');
+console.log(cap);
+
+//------------------------------------------------------------
---
+++ Javascript/string_function.js
@@ -0,0 +1,3 @@
+export const capitilizestring = (str) => {
+    return str.toUpperCase();
+}
